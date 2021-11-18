096b31bc026d16f399ed397b4032e5ae27085e5b
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2021-11-18 20:25:18 -0300

    Correção de posições
    Mudança na posição dos textos

--- payflow/lib/modules/login/login_page.dart
+++ payflow/lib/modules/login/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -27,35 +28,35 @@ class _LoginPageState extends State<LoginPage> {
               color: AppColors.primary,
             ),
             Positioned(
-                top: 40,
+                top: 30,
                 left: 0,
                 right: 0,
                 child: Image.asset(
                   AppImages.person,
                   width: 208,
-                  height: 273,
+                  height: 300,
                 )),
             Positioned(
-              bottom: size.height * 0.15,
+              bottom: size.height * 0.28,
               left: 0,
               right: 0,
               child: Column(
-                mainAxisAlignment: mainAxisAlignment.center,
-                crossAxisAlignment: crossAxisAlignment.center,
-                children: [Image.asset(AppImages.logomini)],
-                Padding(
-                  padding: const EdgeInsets.only(left: 70, right: 70),
-                  child: Text(
-                    "Organize seus boletos em um só lugar",
-                    textAlign: textAlign.center,
-                    style: TextStyles.titleHome,
-                  ),
-                )
-              )
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  )
+                ],
+              ),
             ),
-
-
-
           ],
         ),
       ),
